448996f3445b62b98a331d92ae7dd59db78d2108
MariPires96 <mariana.pires96@hotmail.com>
2026-01-28 17:17:10 -0300

    feat: configura rotas e integra PrivacyPolicy no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import PrivacyPolicy from "./pages/legal/PrivacyPolicy";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Footer />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <main className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
