ebf4372d63e3c724c5a85ce223fcb1aaf5446045
Sandro <sandrolp41@hotmail.com>
2026-01-28 17:34:35 -0300

    feat: implementa category filter e lógica do product list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import './App.css'
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import CategoryFilter from './components/categoryfilter/CategoryFilter';
+import ProductListTest from './components/productlisttest/ProductListTest';
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
+  const [healthyOnly, setHealthyOnly] = useState(false);
 
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route path='/' element={
+        <>
+          <CategoryFilter 
+            onCategoryChange={setSelectedCategory}
+            onHealthyChange={setHealthyOnly}
+          />
+          <ProductListTest 
+            selectedCategory={selectedCategory}
+            healthyOnly={healthyOnly}
+          />
+        </>
+      } />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
