1f590fad8116ecb9333352af8b1b2f318f60a725
MariPires96 <mariana.pires96@hotmail.com>
2026-01-28 16:25:06 -0300

    feat: adiciona componente Footer ao App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <Footer />
       </BrowserRouter>
     </>
   );
